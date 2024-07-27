5a67eae763c80d595789fc6ac568a7e5a8717f84
AlejandroM2023 <alejandromeza437@gmail.com>
2024-07-26 22:10:07 -0500

    format for index.js made

---
+++ index.js
@@ -0,0 +1,18 @@
+//Bring in dependencies
+const fs = require('fs');
+const inquirer = require('inquirer');
+const shapes = require('./lib/shapes');
+
+const questions = [
+]
+
+function makeLogo(){
+
+}
+
+
+function init(){
+    console.log('hi');
+}
+
+init();
