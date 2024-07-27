5bb75ac614d1face17171e1d63ee42364632395d
AlejandroM2023 <alejandromeza437@gmail.com>
2024-07-26 22:45:54 -0500

    questions and validation integrated

--- index.js
+++ index.js
@@ -4,15 +4,45 @@ const inquirer = require('inquirer');
 const shapes = require('./lib/shapes');
 
 const questions = [
+    { 
+        message:'Enter up to three letters for your logo(if more are entered they will be cutoff)',
+        name: 'text',
+        validate: function(input){
+            const done = this.async();
+            if(input.length >3){
+                done('3 characters max');
+                return false;
+            }
+            done (null,true);
+        }
+    },
+    {  
+        message:'Enter color name or hexidecimal value with hashtag',
+        name: 'color',
+    },
+    {  
+        type: 'list',
+        message:'What shape would you like',
+        name: 'shape',
+        choices: ['circle','triangle','square']
+    }
 ]
 
-function makeLogo(){
+function makeLogo(data){
+
+}
+
+function outPutLogo(picture){
 
 }
 
 
 function init(){
-    console.log('hi');
+    inquirer
+        .prompt(questions)
+        .then((answers)=>{
+            makeLogo(answers);
+        })
 }
 
 init();
